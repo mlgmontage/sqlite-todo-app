d19c81eed56b52dc3c87da66595c9e2bd0a788dd
mlgmontage <kettleofrassel1@gmail.com>
2020-06-07 10:18:08 +0500

    cors

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 const sqlite3 = require("sqlite3");
 const morgan = require("morgan");
+const cors = require("cors");
 
 app.use(morgan("dev"));
+app.use(cors());
 
 let db = new sqlite3.Database(
   "./database/todos.db",
