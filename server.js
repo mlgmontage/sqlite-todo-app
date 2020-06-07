852af4f5c9be9c3dc08a195cd0b6d9f808b34c5d
mlgmontage <kettleofrassel1@gmail.com>
2020-06-07 10:14:32 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const sqlite3 = require("sqlite3");
+const morgan = require("morgan");
+
+app.use(morgan("dev"));
+
+let db = new sqlite3.Database(
+  "./database/todos.db",
+  sqlite3.OPEN_READWRITE,
+  (error) => {
+    if (error) {
+      console.error(error.message);
+    }
+    console.log("Connected to the todos database.");
+  }
+);
+
+app.get("/", async (req, res) => {
+  await db.serialize(() => {
+    db.all(`SELECT * FROM todo`, (err, rows) => {
+      if (err) {
+        console.error(err.message);
+      }
+      res.status(200).send(rows);
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`listenning on port ${port}`);
+});
