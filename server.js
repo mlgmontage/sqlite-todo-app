9a97b6c1334c5bef0f9c3c3444ff214acf0fc521
mlgmontage <kettleofrassel1@gmail.com>
2020-11-16 14:40:29 +0500

    run script & setup express & hello route

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const volleyball = require("volleyball");
+const cors = require("cors");
+const port = process.env.PORT || 8080;
+
+const app = express();
+app.use(volleyball);
+app.use(cors());
+
+app.get("/hello", (req, res) => {
+  res.json({
+    message: "world",
+  });
+});
+
+app.listen(port, () =>
+  console.log(`App listening on http://localhost:${port}`)
+);
