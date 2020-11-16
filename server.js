a13dadfca7ede50a7c5106c1987351eb2047f102
mlgmontage <kettleofrassel1@gmail.com>
2020-11-16 15:03:02 +0500

    setup prisma

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const volleyball = require("volleyball");
 const cors = require("cors");
 const port = process.env.PORT || 8080;
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
 app.use(volleyball);
 app.use(cors());
